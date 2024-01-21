98a5985467a0d4d85f42f567b27720975bde2fba
Richie <280645618@qq.com>
2024-01-21 18:46:58 +0800

    update

--- tao-oms/src/main/java/com/qihang/tao/TaoOmsApplication.java
+++ tao-oms/src/main/java/com/qihang/tao/TaoOmsApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * Hello world!
  *
  */
+//@EnableWebSecurity
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TaoOmsApplication
