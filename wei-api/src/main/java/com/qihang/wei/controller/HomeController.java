a3a63207e764a7ed15f9631b0e0945c48967d767
启航 <qiliping@yeah.net>
2024-03-16 17:12:53 +0800

    新增微信视频号api

---
+++ wei-api/src/main/java/com/qihang/wei/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.qihang.wei.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+
+        return "{'code':0,'msg':'wei-api请通过api访问'}";
+    }
+}
